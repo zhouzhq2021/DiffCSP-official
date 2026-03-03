0f86091550152327ce4e80f763a4da9281a99943
Zhongquan Zhou <2240005209@qq.com>
2026-03-03 14:13:20 +0800

    Refactor StructureGraph initialization method: from_local_env_strategy -> with_local_env_strategy

--- diffcsp/common/data_utils.py
+++ diffcsp/common/data_utils.py
@@ -176,11 +176,11 @@ def build_crystal_graph(crystal, graph_method='crystalnn'):
 
     if graph_method == 'crystalnn':
         try:
-            crystal_graph = StructureGraph.from_local_env_strategy(
+            crystal_graph = StructureGraph.with_local_env_strategy(
                 crystal, CrystalNN)
         except:
             crystalNN_tmp = local_env.CrystalNN(distance_cutoffs=None, x_diff_weight=-1, porous_adjustment=False, search_cutoff=10)
-            crystal_graph = StructureGraph.from_local_env_strategy(
+            crystal_graph = StructureGraph.with_local_env_strategy(
                 crystal, crystalNN_tmp) 
     elif graph_method == 'none':
         pass
